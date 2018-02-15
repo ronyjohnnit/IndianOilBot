27aa6775b54e468c7a7ac8025283d127c8829956
ronyjohnnit <ronyjohnnit@gmail.com>
2018-02-15 18:19:44 +0530

    rivescript integrated

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 var express = require('express');
 var app = express();
-var fs = require("fs");
+//var fs = require("fs");
+
+RiveScript = require("rivescript");
+
+// Create the bot.
+var bot = new RiveScript();
+
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/listUsers', function (req, res) {
-   fs.readFile("users.json", 'utf8', function (err, data) {
-       console.log( data );
+app.post('/listUsers', function (req, res) {
+   /*fs.readFile("users.json", 'utf8', function (err, data) {
+
+      console.log( "Request="+req);
+       console.log( "Post data="+req.body.text );
        res.end( data );
-   });
+   });*/
+   res.end(req.body.text);
 })
 
 var server = app.listen(app.get('port'), function () {
