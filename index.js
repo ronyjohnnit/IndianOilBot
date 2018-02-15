535f5bac92fe11198bfd88dcefce004cafb40ba6
ronyjohnnit <ronyjohnnit@gmail.com>
2018-02-15 13:57:28 +0530

    port changed to 5000

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var fs = require("fs");
 
+app.set('port', (process.env.PORT || 5000));
+
 app.get('/listUsers', function (req, res) {
    fs.readFile("users.json", 'utf8', function (err, data) {
        console.log( data );
@@ -9,7 +11,7 @@ app.get('/listUsers', function (req, res) {
    });
 })
 
-var server = app.listen(8081, function () {
+var server = app.listen(app.get('port'), function () {
 
   var host = server.address().address
   var port = server.address().port
